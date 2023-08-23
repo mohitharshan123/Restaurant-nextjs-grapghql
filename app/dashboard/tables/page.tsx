abaf11476739a8383d64b6135672a309aa62eb63
Mohit <mohitharshan.mec@gmail.com>
2023-08-23 15:28:33 +0530

    seperate component for qr scanner

--- app/dashboard/tables/page.tsx
+++ app/dashboard/tables/page.tsx
@@ -2,28 +2,26 @@
 
 import React, { useCallback, useEffect, useState } from "react";
 import classnames from "classnames";
-import { Stepper, Step, Option, Select, Card, CardBody, Typography, List, ListItem, ListItemPrefix, Button, Drawer } from "@material-tailwind/react";
+import { Card, CardBody, Typography } from "@material-tailwind/react";
 import { UseQueryResult } from "react-query";
 import { useQRCode } from 'next-qrcode';
 
 import { useMyRestaurant, useUpdateFloorPlan } from "../../hooks/api/useRestaurantApi";
 import LeftBar from "./left-bar";
+import QRDrawer from "./qr-drawer";
 
 const Tables = () => {
   const [selectedFloor, setSelectedFloor] = useState<number>(1);
 
   const [noOfFloors, setNoOfFloors] = useState<string | undefined>("1")
   const [tableCount, setTableCount] = useState<{ [key: string]: string }>({});
-  const [tablePositions, setTablePositions] = useState<{ [key: string]: Number[] }>({})
-  const [isQRDrawerOpen, setIsQRDrawerOpen] = useState<boolean>(false)
+  const [tablePositions, setTablePositions] = useState<{ [key: string]: Number[] }>({});
+  const [isQRDrawerOpen, setIsQRDrawerOpen] = useState<boolean>(false);
 
   const { data: restaurant } = useMyRestaurant() as UseQueryResult<{
     name: string;
     floorPlan: Record<string, number[]>
   }>;
-
-  const { Canvas } = useQRCode();
-
 
   useEffect(() => {
     if (!restaurant?.floorPlan) return;
@@ -65,22 +63,6 @@ const Tables = () => {
     [selectedFloor, tableCount]
   );
 
-  const handlePrint = (tableNumber: number) => {
-    const bodyElement = document.getElementsByTagName('body')[0];
-    if (!bodyElement) return;
-    const printArea = document.getElementById(`qr-print-${tableNumber}`)
-    const allPrintAreas = document.querySelectorAll('[id^="qr-print-"]');
-    allPrintAreas.forEach((printArea) => {
-      printArea.setAttribute("style", "visibility: hidden;");
-    });
-    printArea?.setAttribute("style", "visibility: visible; position: fixed; top: 5px;");
-    bodyElement.classList.add('printing');
-    window.print();
-    bodyElement.classList.remove('printing');
-    allPrintAreas.forEach((printArea) => {
-      printArea.setAttribute("style", "visibility: visible;");
-    });
-  }
 
   const leftBarProps = {
     noOfFloors, setNoOfFloors, tableCount, setTableCount,
@@ -105,26 +87,7 @@ const Tables = () => {
       </div>
     </div>
   </div>
-    <Drawer placement="right" open={isQRDrawerOpen} onClose={() => setIsQRDrawerOpen(false)} overlay={false} className="overflow-auto p-4 items-center flex flex-col space-y-10">
-      {tablePositions[selectedFloor]?.map((tableNumber: any) => (<div className="flex items-center flex-col">
-        <div className="flex items-center flex-col p-10 bg-amber-200 rounded-xl m-2" id={`qr-print-${tableNumber}`}>
-          <Typography variant="h3">{restaurant?.name}</Typography>
-          <Typography variant="h6">{`Table ${tableNumber + 1}`}</Typography>
-          <Canvas
-            text={`https://www.dine.com/${restaurant?.name}/guest/table/{tableNumber}`}
-            options={{
-              errorCorrectionLevel: 'M',
-              margin: 3,
-              scale: 4,
-              width: 200,
-              color: {
-                dark: "#000000",
-                light: "#FFFFFF",
-              },
-            }}
-          /></div><Button className="rounded-xl" onClick={() => handlePrint(tableNumber)}>Print</Button></div>
-      ))}
-    </Drawer>
+    <QRDrawer {...{ isQRDrawerOpen, setIsQRDrawerOpen, restaurant: { name: restaurant?.name, floorPlan: restaurant?.floorPlan }, tablePositions, selectedFloor }} />
   </>;
 };
 
---
+++ app/dashboard/tables/qr-drawer.tsx
@@ -0,0 +1,58 @@
+import { Typography, Button, Drawer } from "@material-tailwind/react";
+import { Restaurant } from "main/restaurant/restaurant.schema";
+import { useQRCode } from "next-qrcode";
+
+type QRDrawerProps = {
+    isQRDrawerOpen: boolean;
+    setIsQRDrawerOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    restaurant: {
+        name: string | undefined;
+        floorPlan: Record<string, number[]> | undefined;
+    };
+    tablePositions: { [key: string]: Number[] };
+    selectedFloor: number;
+}
+
+const QRDrawer: React.FC<QRDrawerProps> = ({ isQRDrawerOpen, setIsQRDrawerOpen, restaurant, tablePositions, selectedFloor }) => {
+    const { Canvas } = useQRCode();
+
+    const handlePrint = (tableNumber: number) => {
+        const bodyElement = document.getElementsByTagName('body')[0];
+        if (!bodyElement) return;
+        const printArea = document.getElementById(`qr-print-${tableNumber}`)
+        const allPrintAreas = document.querySelectorAll('[id^="qr-print-"]');
+        allPrintAreas.forEach((printArea) => {
+            printArea.setAttribute("style", "visibility: hidden;");
+        });
+        printArea?.setAttribute("style", "visibility: visible; position: fixed; top: 5px;");
+        bodyElement.classList.add('printing');
+        window.print();
+        bodyElement.classList.remove('printing');
+        allPrintAreas.forEach((printArea) => {
+            printArea.setAttribute("style", "visibility: visible;");
+        });
+    }
+
+    return <Drawer placement="right" open={isQRDrawerOpen} onClose={() => setIsQRDrawerOpen(false)} overlay={false} className="overflow-auto p-4 items-center flex flex-col space-y-10">
+        {tablePositions[selectedFloor]?.map((tableNumber: any) => (<div className="flex items-center flex-col">
+            <div className="flex items-center flex-col p-10 bg-amber-200 rounded-xl m-2" id={`qr-print-${tableNumber}`}>
+                <Typography variant="h3">{restaurant?.name}</Typography>
+                <Typography variant="h6">{`Table ${tableNumber + 1}`}</Typography>
+                <Canvas
+                    text={`https://www.dine.com/${restaurant?.name}/guest/table/{tableNumber}`}
+                    options={{
+                        errorCorrectionLevel: 'M',
+                        margin: 3,
+                        scale: 4,
+                        width: 200,
+                        color: {
+                            dark: "#000000",
+                            light: "#FFFFFF",
+                        },
+                    }}
+                /></div><Button className="rounded-xl" onClick={() => handlePrint(tableNumber)}>Print</Button></div>
+        ))}
+    </Drawer>
+}
+
+export default QRDrawer;
