1519130ec1c808d49447bdbd6f797df87811f875
Mohit <mohitharshan.mec@gmail.com>
2023-09-04 13:34:21 +0530

    minor changes

--- app/actions.ts
+++ app/actions.ts
@@ -3,5 +3,11 @@
 import { cookies } from 'next/headers'
 
 export async function logout() {
-    await cookies().set("accessToken", '')
+    await cookies().set("accessToken", '', {
+        maxAge: 3.154e10,
+        domain: process.env.DOMAIN,
+        path: "/",
+        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production"
+    })
 }
